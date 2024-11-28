ba32cb79aad6448c12f3fb2eccccd4a87c102d05
Nicolaskkkkkkjjj <127906077+Nicolaskkkkkkjjj@users.noreply.github.com>
2024-11-28 02:03:14 -0300

    isso ae

---
+++ Gabrielesellee/SCA/js/script.js
@@ -0,0 +1,10 @@
+function criarConta() {
+    alert("Redirecionando para a página de criação de conta.");
+    window.location.href = "create-account.html";
+}
+
+function entrarComoConvidado() {
+    alert("Entrando como convidado.");
+    window.location.href = "pagina_inicial.html";
+}
+
---
+++ Gabrielesellee/teste/script.js
@@ -0,0 +1,11 @@
+// script.js
+
+function criarConta() {
+    alert("Redirecionando para a página de criação de conta.");
+    window.location.href = "create-account.html";
+}
+
+function entrarComoConvidado() {
+    alert("Entrando como convidado.");
+    window.location.href = "pagina_inicial.html";
+}
